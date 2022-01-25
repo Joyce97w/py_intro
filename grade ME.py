2ef086503097eb2681f22275fc3296de4e26cfe5
Joyce97w <98325097+Joyce97w@users.noreply.github.com>
2022-01-25 21:34:16 +0800

    Add files via upload

---
+++ grade ME.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 21:17:34 2022
+
+@author: acer
+"""
+M=input("數學成績?")
+M=int(M)
+E=input("英文成績?")
+E=int(E)
+if M>=90 and E>=90 or M=100 or E=100:
+    print("拿到獎學金")
